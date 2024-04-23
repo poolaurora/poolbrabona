9ca84309847bb823039d562c0a51f54d8aee0a47
dnkzineo <pamelatomazbotmines@gmail.com>
2024-04-23 18:43:28 -0300

    Meta px update

--- resources/views/checkout/sucess.blade.php
+++ resources/views/checkout/sucess.blade.php
@@ -27,7 +27,7 @@
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '739262591656106');
+            fbq('init', '406273602349612');
             fbq('track', 'PageView');
             // Disparar evento de purchase
             fbq('track', 'Purchase', {
@@ -56,7 +56,7 @@
             });
             </script>
             <noscript><img height="1" width="1" style="display:none"
-            src="https://www.facebook.com/tr?id=739262591656106&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=406273602349612&ev=PageView&noscript=1"
             /></noscript>
         <!-- End Meta Pixel Code -->
 </head>
