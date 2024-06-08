a1d873083276084386167ebf6ad27c889cb86d8e
dnkzineo <pamelatomazbotmines@gmail.com>
2024-06-08 12:56:37 -0300

    Cache opt

--- app/Http/Middleware/TrackOnlineUsers.php
+++ app/Http/Middleware/TrackOnlineUsers.php
@@ -1,10 +1,10 @@
 <?php
+
 namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log;
 
 class TrackOnlineUsers
 {
@@ -14,23 +14,25 @@ class TrackOnlineUsers
         $sessionId = session()->getId();
         $current_page = $request->path();
 
-        // Atualizar cache com usuários online
+        // Obter o cache de usuários online
         $onlineUsers = Cache::get('online-users', []);
 
+        // Adicionar ou atualizar o usuário atual
         $onlineUsers[$sessionId] = [
             'name' => $user ? $user->name : 'Guest',
             'current_page' => $current_page,
             'last_activity' => now()
         ];
 
-        Cache::put('online-users', $onlineUsers, now()->addMinutes(5));
+        // Remover sessões antigas (opcional, mas recomendado)
+        $expirationTime = now()->subMinutes(1);
+        $onlineUsers = array_filter($onlineUsers, function ($user) use ($expirationTime) {
+            return $user['last_activity'] > $expirationTime;
+        });
+
+        // Atualizar o cache com a nova lista de usuários online
+        Cache::put('online-users', $onlineUsers, now()->addMinutes(1));
 
         return $next($request);
     }
 }
-
-
-
-
-
-
